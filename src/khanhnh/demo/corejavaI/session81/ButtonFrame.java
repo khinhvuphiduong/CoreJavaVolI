fdcb8355fbe8257a1b355aade92eab9ca92ba74b
Nguyen Huy Khanh <khanhnguyenhuy@hotmail.com>
2016-06-15 08:39:42 +0700

    khanh update

--- src/khanhnh/demo/corejavaI/session81/ButtonFrame.java
+++ src/khanhnh/demo/corejavaI/session81/ButtonFrame.java
@@ -7,14 +7,27 @@ import java.awt.FlowLayout;
 import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.event.WindowStateListener;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ActionMap;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.InputMap;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.ChangeEvent;
 import javax.swing.JToolBar;
+import javax.swing.KeyStroke;
 
 public class ButtonFrame extends JFrame {
 
@@ -34,7 +47,8 @@ public class ButtonFrame extends JFrame {
 					ButtonFrame frame = new ButtonFrame();
 					//Show frame center of screen
 					frame.setLocationRelativeTo(null);
-					frame.setVisible(true);
+					
+										frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -46,6 +60,7 @@ public class ButtonFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public ButtonFrame() {
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 400);
 		
@@ -58,11 +73,21 @@ public class ButtonFrame extends JFrame {
 		JButton btnBlue=new JButton("Blue");
 		btnBlue.setBounds(240, 20, 100, 50);
 		
+		ColorAction blueaction=new ColorAction("Blue", new ImageIcon("index.jpg"), Color.BLUE);
+		btnBlue.setAction(blueaction);
+		
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		
 		//fix last button always fills layout
 		contentPane.setLayout(new FlowLayout());
+		
+		ColorAction yellowAction=new ColorAction("Yellow Action", new ImageIcon("index.jpg"), Color.YELLOW);
+		InputMap imap=contentPane.getInputMap(JComponent.WHEN_FOCUSED);
+		imap.put(KeyStroke.getKeyStroke("ctrl Y"), "panel.yellow");
+		ActionMap amap=contentPane.getActionMap();
+		amap.put("panel.yellow", yellowAction);
 		
 		contentPane.add(btnYellow);
 		contentPane.add(btnRed);
@@ -71,7 +96,6 @@ public class ButtonFrame extends JFrame {
 		
 		//ColorAction yellowaction = new ColorAction(Color.yellow, btnYellow);
 		//btnYellow.addActionListener(yellowaction);
-		
 		
 		setContentPane(contentPane);
 		//Button not fills panel
@@ -95,6 +119,74 @@ public class ButtonFrame extends JFrame {
 //		}
 //		
 //	}
+	
+	class Terminator implements WindowListener{
+
+
+		@Override
+		public void windowActivated(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void windowClosed(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void windowClosing(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowDeactivated(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowDeiconified(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowIconified(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowOpened(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	public class ColorAction extends AbstractAction{
+		
+		public ColorAction(String name, Icon icon, Color c){
+			putValue(Action.NAME, name);
+			putValue(Action.SMALL_ICON, icon);
+			putValue("color", c);
+			putValue(Action.SHORT_DESCRIPTION, "Set Panel color to"+name.toLowerCase());
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			Color c=(Color)getValue("color");
+			
+			JPanel  panel=(JPanel )e.getSource();
+			//JPanel frame=(JPanel)btn.getParent();
+			panel.setBackground(c);
+			
+			//JOptionPane.showMessageDialog(null, btn.getpa);
+		}
+	}
 }
+
 
 
